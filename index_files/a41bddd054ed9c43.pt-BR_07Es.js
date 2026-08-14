2c22595d9f8486d9df710f86962c9d5629a33a8e
lojabomdesconto <81715476+lojabomdesconto@users.noreply.github.com>
2026-08-14 09:54:22 -0300

    Add files via upload

---
+++ index_files/a41bddd054ed9c43.pt-BR_07Es.js
@@ -0,0 +1,2 @@
+window['cmsg']['assets'] = window['cmsg']['assets'] || {};
+window['cmsg']['assets']["pt-BR"] = Object.assign(window['cmsg']['assets']["pt-BR"] || {}, {"15706":{"js":["6018f2c7a77c016c.strings.js"],"css":[]},"38701":{"js":["77822fb9a531f9b2.strings.js"],"css":[]}});
